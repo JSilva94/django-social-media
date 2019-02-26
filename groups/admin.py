5f34b0738f1d5ed10bfcf320be4444e232a81450
Jayde Silva <jayde.silva@mail.utoronto.ca>
2019-02-26 18:24:59 -0500

    views and models for posts and groups

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Group, GroupMember
+
+class GroupMemberinline(admin.TabularInline):
+    model = GroupMember
+
+admin.site.register(Group)
